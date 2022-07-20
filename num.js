b2d7088c7ea9c510edd0eae7a7d5aac80aa58c65
zzinggu636 <109051977+zzinggu636@users.noreply.github.com>
2022-07-20 12:08:16 +0900

    Add files via upload

---
+++ num.js
@@ -0,0 +1,16 @@
+let oct = 015;
+let hex = 0x15;
+let condition = true;
+
+document.write("8진수 015는 십진수로" + oct +"<br>")
+document.write("16진수 0x15는 십진수로" + hex +"<br>")
+document.write("condition" = condition +"<br>")
+document.write('문자열 단일 인용부로도 표현<br>')
+document.write("그는| '그녀는 누구세요|'라고 물었다.")
+document.write("줄바꿈은 <br> 새로운 줄에 출력 <br>")
+document.write("그는| '그녀는 누구세요|'라고 물었다." = condition +"<br>")
+
+
+
+
+
---
+++ var.js
@@ -0,0 +1,25 @@
+let x=1;
+console.log(x);
+
+function f(){
+    let y;
+    x =10;
+    y=20;
+    z=30;
+    console.log(x);
+    console.log(y);
+    console.log(z);
+
+    if(y == 20){
+        let b = 40;
+        b++;
+    console.log(b);
+        {
+        let c;
+        c=1;
+        }
+    }
+}
+f();
+console.log(y);
+console.log(x);
